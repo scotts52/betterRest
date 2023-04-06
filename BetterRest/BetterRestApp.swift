be5aae350430110ff9eca712d62cdd555d5c5dd1
scotts52 <scotty.stevenson123@gmail.com>
2023-04-06 09:19:55 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Stevenson, Scott on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
